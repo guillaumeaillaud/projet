cf891fae42f627e222cf0aeb46baacbff556aaa2
guillaumeaillaud <guillaume.aillaud.13@gmail.com>
2020-02-11 17:32:55 +0100

    php-boucle-for

--- ONEPAGE/php/view/section-index.php
+++ ONEPAGE/php/view/section-index.php
@@ -1,37 +1,35 @@
+<section>
+    <div class="image">
+        <?php
+        $logo = ["logohtml.jpg", "logocss.jpg", "logophp.jpg"];
 
-        <section>
-            <div class="image">
-                <?php
-                    $logo = ["logohtml.jpg","logocss.jpg","logophp.jpg"];
-
-                    foreach ($logo as $different)
-                    {
-                        echo <<<html
+        foreach ($logo as $different) {
+            echo <<<html
                         <img src="assets/img/$different" alt="">
                         html;
-                    }
-                  
-                ?>
-            </div>
-        </section>
+        }
 
-            <section>
+        $moto = ["moto4.jpg", "moto5.jpg", "moto6.jpg"];
+        $i = 0;
+        while ($i < count($moto)) {
+            echo <<<html
+                      <img src="assets/img/$moto[$i]" alt="">
+                     html;
+            $i = $i + 1;
+        }
+        ?>
+    </div>
+</section>
+
+<!--  <section>
+
+            
                 <div class="image2">
-                    <?php
-                        $moto = ["moto4.jpg","moto5.jpg","moto6.jpg"];
-                        
-                        foreach ($moto as $changement)
-                        {
-                            echo <<<html
-                            <img src="assets/img/$changement" alt="">
-                            html;
-                        }
-
-                    ?>
-             <!--   <img src="assets/img/moto4.jpeg" alt="">
-                <img src="assets/img/moto5.jpeg" alt="">
-                <img src="assets/img/moto6.jpeg" alt="">
-                    -->   
+           
+                <img src="assets/img/moto4.jpg" alt="">
+                <img src="assets/img/moto5.jpg" alt="">
+                <img src="assets/img/moto6.jpg" alt="">
+              
 
                 </div>
 
