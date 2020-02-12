5fc0b983b3f3821c9c33a8eea202802ce3f72d17
guillaumeaillaud <guillaume.aillaud.13@gmail.com>
2020-02-12 16:18:12 +0100

    ajout de colonnes en dessous des images

--- ONEPAGE/php/view/section-index.php
+++ ONEPAGE/php/view/section-index.php
@@ -2,18 +2,34 @@
     <div class="image">
         <?php
         $logo = ["logohtml.jpg", "logocss.jpg", "logophp.jpg"];
+        $texte1 = <<<texte
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odio, non nihil architecto, maxime cupiditate sit, voluptate modi delectus exercitationem eos reiciendis magnam veniam. Cupiditate minima, itaque blanditiis modi sint voluptas?
+        texte;
+        $texte2 = <<<texte
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odio, non nihil architecto, maxime cupiditate sit, voluptate modi delectus exercitationem eos reiciendis magnam veniam. Cupiditate minima, itaque blanditiis modi sint voluptas?
+        texte;
+        $texte3 = <<<texte
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odio, non nihil architecto, maxime cupiditate sit, voluptate modi delectus exercitationem eos reiciendis magnam veniam. Cupiditate minima, itaque blanditiis modi sint voluptas?
+        texte;
+        $paragraphe = [$texte1, $texte2, $texte3];
 
-        foreach ($logo as $different) 
+        foreach ($logo as $indice => $different) 
         {
             echo <<<html
-                        <img src="assets/img/$different" alt="">
-                        html;
+
+            <div class="colonne">
+                <img src="assets/img/$different" alt="">
+                <p>$paragraphe[$indice]</p>
+            </div>
+
+            html;
         }
          ?>
     </div>
 </section>
 <section id="s5">
         <h1>CONTACT</h1>
+        
         <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odio, non nihil architecto, maxime cupiditate sit, voluptate modi delectus exercitationem eos reiciendis magnam veniam. Cupiditate minima, itaque blanditiis modi sint voluptas?</p>
 
         <form action="#s5" method="GET">
