f2fde6ddfa22b18bc458a5bb3e3597b71e95b51e
guillaumeaillaud <guillaume.aillaud.13@gmail.com>
2020-02-14 12:23:37 +0100

    ajout de validation et nettoyage donnees

--- jour11/form_validation/index.php
+++ jour11/form_validation/index.php
@@ -1,20 +1,41 @@
 <?php
+    require_once 'lib/validator.php';
 
     if (!empty($_REQUEST)) {
-        $username = $_REQUEST['username'];
-        $email = $_REQUEST['email'];
-        $pwd = $_REQUEST['password'];
-        $cfm_pwd = $_REQUEST['confirm_pwd'];
+        // $username = $_REQUEST['username'];
+        // $email = $_REQUEST['email'];
+        // $pwd = $_REQUEST['password'];
+        // $cfm_pwd = $_REQUEST['confirm_pwd'];
+
+        // nettoyage des inputs
+        // $username = trim($username);
+        // $username = htmlspecialchars($username);
+        // $username = stripslashes($username);
+
+        // $username = stripslashes(htmlspecialchars(trim($_REQUEST['username'])));
+        // $email = stripslashes(htmlspecialchars(trim($_REQUEST['email'])));
+        // $pwd = stripslashes(htmlspecialchars(trim($_REQUEST['password'])));
+        // $cfm_pwd = stripslashes(htmlspecialchars(trim($_REQUEST['confirm_pwd'])));
+
+        $username = validateInput($_REQUEST['username']);
+        $email = validateInput($_REQUEST['email']);
+        $pwd = validateInput($_REQUEST['password']);
+        $cfm_pwd = validateInput($_REQUEST['confirm_pwd']);
+
+        $match = comparePwd($pwd, $cfm_pwd);
+
+        var_dump($match);
+
+        // var_dump($username);
 
         // echo '<pre>';
         // print_r($_REQUEST);
         // echo '</pre>';
-
+        //
         // nettoyage des inputs
     }
 
 ?>
-
 <!DOCTYPE html>
 <html lang="fr">
 <head>
