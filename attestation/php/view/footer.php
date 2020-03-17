3c580c668b9ab91a1491bf55dd0b6f40f6339d91
guillaumeaillaud <guillaume.aillaud.13@gmail.com>
2020-03-17 17:15:40 +0100

    attestation formulaire connecte a bd SQL

---
+++ attestation/php/view/footer.php
@@ -0,0 +1,11 @@
+</main>
+    <footer>
+        <nav>
+            <a href="verifier.php">verifier</a>
+            <a href="admin.php">admin</a>
+        </nav>
+    </footer>
+    <script src="assets/js/app.js"></script>
+</body>
+
+</html>
---
+++ attestation/php/view/section-index.php
@@ -0,0 +1,6 @@
+
+        <section>
+            <h2>COMMENT OBTENIR SON ATTESTATION</h2>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quae temporibus dignissimos sapiente modi, sit quisquam voluptate amet atque! Quis facilis assumenda minima error sint a tempora maxime porro consequuntur aut.</p>
+        </section>
+    
