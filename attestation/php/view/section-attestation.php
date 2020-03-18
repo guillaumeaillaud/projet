6ab800d41d6e4535c22d110183921837dd799873
guillaumeaillaud <guillaume.aillaud.13@gmail.com>
2020-03-18 11:44:32 +0100

    attestation formulaire connecte a bd SQL modifs

--- attestation/php/view/section-attestation.php
+++ attestation/php/view/section-attestation.php
@@ -3,7 +3,7 @@
     <form action="" method="POST">
         <input type="text" name="nom" required placeholder="votre nom">
         <input type="text" name="prenom" required placeholder="votre prenom">
-        <textarea name="adress" cols="60" rows="6" required placeholder="votre adresse"></textarea>
+        <textarea name="adresse" cols="60" rows="6" required placeholder="votre adresse"></textarea>
         <h3>cocher la raison de votre deplacement</h3>
 
         <label>
