3a2fae0d7b2177c0e4bb9c6d83b1c65fc764b23c
guillaumeaillaud <guillaume.aillaud.13@gmail.com>
2020-03-26 15:22:36 +0100

    connexion a une bdd api avec js

--- exos/AJAX/app.js
+++ exos/AJAX/app.js
@@ -11,44 +11,55 @@ function capitalize(str){
     return str[0].toUpperCase() + str.slice(1);
 }
 
-function main(){
-    // 1. Choper l'adresse IP du PC qui ouvre la page:
-    //https://api.ipify.org?format=json
-    fetch('https://api.ipify.org?format=json')
-        .then(function(response){
-            return response.json();
-        })
+ // step 1 : obtenir son adresse ip
+ fetch("https://api.ipify.org?format=json")
+ // version javascript classique
+ .then(function(response) {
+   return response.json();
+ })
+ // version ES6 (fonctions fléchées)
+ // si on a qu'un seul paramètre à passer à la fonction on peut se passer des parenthèses. Elles seront obligatoires s'il y en a plusieurs
+ // si la fonction n'exécute qu'une seule instruction, on peut aussi se passer des {}. Sinon elles seront obligatoires
+ // dans le code à la ligne suivante le return est implicite, on n'a pas besoinde le spécifier
+ // .then(response => response.json())
 
-        .then(function(data){
-            const ip = data.ip
+ .then(function(data) {
+   const ip = data.ip;
 
-            // 2. Choper la ville grace a l'adrsse IP:
-    //http://freegeoip.net/json/adresseIPDuMec
-    //utilisation des backsticks string litterals
-    //fetch(`http://api.ipstack.com/${ip}?access_key=30c029b1d2a8dcdcdf26ac5a0e07b914`)
+   // step 2 :  récupérer un nom de ville en fonction de l'adresse ip obtenue
+   // utilisation des backticks string litterals
+   // fetch(
+   //   `http://api.ipstack.com/${ip}?access_key=30c029b1d2a8dcdcdf26ac5a0e07b914`
+   // );
 
-    //methode classique avec concatenation de chaines de caractere
-    fetch("http://api.ipstack.com/" + ip + "?access_key=30c029b1d2a8dcdcdf26ac5a0e07b914")
+   // méthode classique avec concaténation de chaînes de caractères
+   fetch(
+     "http://api.ipstack.com/" +
+       ip +
+       "?access_key=30c029b1d2a8dcdcdf26ac5a0e07b914"
+   )
+     .then(function(response) {
+       return response.json();
+     })
+     .then(function(data) {
+       let city = data.city; // cici on va récupérer Marseille 01 qu'on doit transformer en marseille
 
-            .then(function(response){
-                return response.json();
-            })
-            .then(function(data){
-                let city = data.city; // ici on va recup Marseille 01 qu'on doit transformer en Marseille
-                // on ne doit conserver que le nom de la ville
-                // on doit passer la chaine de caractere en minuscules
-                console.log(city);
-            });
-        });
-    // 2. Choper la ville grace a l'adrsse IP:
-    //http://freegeoip.net/json/adresseIPDuMec
+       console.log(city);
+       // on ne doit conserver que le nom de la ville
+       // on doit passer la chaîne de caractères en minuscules
+       city = city.split(" ")[0].toLowerCase();
 
+       // step 3 : récupérer les conditions météo de la ville
+       fetch(
+         `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=8e602b9ea28ed4f9f8fc97a5f6d1105c&lang=fr&units=metric`
+       )
+         .then(function(response) {
+           return response.json();
+         })
+         .then(function(data) {
+           console.log(data);
+         });
+    });
+ });
 
-    // 3. Choper les infos météo grace à la ville:
-    //https://api.openweathermap.org/data/2.5/weather?q=VilleDuMec&appid=8e602b9ea28ed4f9f8fc97a5f6d1105c&lang=fr&units=metric
-
-
-    // 4. Afficher les informations sur la page
-}
-
-main();
+// step 3 : récupérer les conditions météo de la ville
