83555442f2b06fa2213263e7dce4b240284f2574
guillaumeaillaud <guillaume.aillaud.13@gmail.com>
2020-05-13 11:23:00 +0200

    cms-poo rajout

---
+++ cms-poo/php/view/template-admin.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <h1>TITRE1</h1>
+        <nav>
+            <a href="index.php">accueil</a>
+            <a href="presentation.php">présentation</a>
+            <a href="contact.php">contact</a>
+            <a href="admin.php">admin</a>
+        </nav>
+    </header>
+    <main>
+        <h1>PAGE ADMIN...</h1>
+    </main>
+</body>
+</html>
