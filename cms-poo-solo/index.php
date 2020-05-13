fa3f7101c9f7072ebd3b33d8015c054feeabf5c9
guillaumeaillaud <guillaume.aillaud.13@gmail.com>
2020-05-13 17:33:03 +0200

    modif cmsPoo creation

--- cms-poo-solo/index.php
+++ cms-poo-solo/index.php
@@ -6,7 +6,7 @@
 // pour commencer on charge a la main avec require_once
 require_once "php/class/App.php";
 
-// maintenant on peut faire d ela POO
+// maintenant on peut faire de la POO
 // le plus simple est de faire de la programmation par classe
 
 App::start();
